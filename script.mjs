00215bb77828329d56a01c40008d5d13a5dacd14
FireIsGood <fireisadequate@gmail.com>
2025-03-17 16:23:08 -0700

    feat: svgs that work

--- script.mjs
+++ script.mjs
@@ -1,4 +1,14 @@
+function processSvgs(wrapper, svgDefinitions) {
+  const toReplace = wrapper.find("use[href^='#']");
+  toReplace.replaceWith(function (i) {
+    const iconId = $(toReplace[i]).attr("href");
+    const svgReplacement = svgDefinitions.find(iconId).clone();
+    return svgReplacement ?? toReplace[i];
+  });
+}
+
 $(async function () {
+  const svgDefinitions = $("#svg-definitions");
   const postHtml = $("#post-html");
   const postWrapper = $("#post-wrapper");
   const postContainer = $("#post-wrapper-inner");
@@ -8,14 +18,14 @@ $(async function () {
   postHtml.on("input", function () {
     postContainer.empty();
     postContainer.append(postHtml.val());
+    processSvgs(postContainer, svgDefinitions);
   });
 
   screenshotPostButton.click(function () {
     html2canvas(postWrapper[0], { allowTaint: true, scale: 1 }).then(function (canvas) {
       canvas.style.width = null;
       canvas.style.height = null;
-      postImageContainer.empty();
-      postImageContainer.append(canvas);
+      postImageContainer.empty().append(canvas);
     });
   });
 
@@ -23,8 +33,6 @@ $(async function () {
     url: "managed.svg",
     dataType: "html",
   }).done(function (svgData) {
-    $("head").append(svgData);
+    svgDefinitions.empty().append(svgData);
   });
-
-  canvg();
 });
